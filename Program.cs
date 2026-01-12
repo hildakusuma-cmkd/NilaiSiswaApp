a192bc51aee5b1cd52c685225c2fe24b692aa755
hildakusuma-cmkd <ririnsetyawati405@gmail.com>
2026-01-12 10:03:16 +0700

    Tambah input bilai & nama

--- Program.cs
+++ Program.cs
@@ -3,4 +3,11 @@ string nama;
 
 Console.WriteLine("Aplikasi Nilai Siswa");
 
+Console.Write("Input Nama ");
+nama = Console.ReadLine();
+
+Console.Write("Input Nilai ");
+nilai = int.Parse(Console.ReadLine());
+
+
 
