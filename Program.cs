d6ec85d00a8f98baeff12613aae891325b82f3b0
hildakusuma-cmkd <ririnsetyawati405@gmail.com>
2026-01-12 10:03:57 +0700

    Tambah validasi nilai

--- Program.cs
+++ Program.cs
@@ -9,5 +9,9 @@ nama = Console.ReadLine();
 Console.Write("Input Nilai ");
 nilai = int.Parse(Console.ReadLine());
 
+if (nilai >= 75)
+    Console.WriteLine("Selamat, Anda Lulus!");
+else
+    Console.WriteLine("Maaf, Anda Belum Lulus");
 
 
